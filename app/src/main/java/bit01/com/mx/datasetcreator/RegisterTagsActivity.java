10d2fcd42210af335c4ec5ef32e3f3030133802a
Rodrigo Medina <rodrigo.medina.neri@gmail.com>
2017-05-26 17:30:08 -0500

    First commit

---
+++ app/src/main/java/bit01/com/mx/datasetcreator/RegisterTagsActivity.java
@@ -0,0 +1,13 @@
+package bit01.com.mx.datasetcreator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RegisterTagsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_tags);
+    }
+}
